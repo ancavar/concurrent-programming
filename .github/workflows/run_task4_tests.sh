b23a0422189cd032de689db2cf6fa037228758c8
ancavar <danaj2004@yandex.ru>
2024-05-31 00:55:35 +0300

    add ci to task4

---
+++ .github/workflows/run_task4_tests.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+REPEAT_COUNT=3
+
+for i in $(seq 1 $REPEAT_COUNT)
+do
+  echo "Run #$i"
+  ./gradlew test
+  if [ $? -ne 0 ]; then
+    echo "Test run #$i failed."
+    exit 1
+  fi
+done
